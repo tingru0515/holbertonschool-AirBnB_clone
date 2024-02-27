4703d9177036f6c73f8d8c7682e9700573215090
tingru0515 <tingru0515@gmail.com>
2024-02-27 21:24:16 +1100

    reload obj same as create error

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
+import models
 import uuid
 from datetime import datetime
+from models.engine.file_storage import FileStorage
 
 
 class BaseModel:
@@ -24,13 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self._save_to_storage()
-
-    def _save_to_storage(self):
-        """Save the current instance to storage."""
-        from models.engine.file_storage import FileStorage
-        storage = FileStorage()
-        storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
