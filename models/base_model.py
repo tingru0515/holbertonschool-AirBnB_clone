59d15b9430ab683958cd96bff817cedeb929b097
tingru0515 <tingru0515@gmail.com>
2024-02-27 21:37:14 +1100

    error reload

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
-import models
 import uuid
 from datetime import datetime
 
@@ -25,7 +24,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            self._save_to_storage()
+
+    def _save_to_storage(self):
+        """Save the current instance to storage."""
+        from models.engine.file_storage import FileStorage
+        storage = FileStorage()
+        storage.reload()
+        storage.new(self)
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
