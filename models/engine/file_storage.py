caeacaf37a47e8556bd1ae14d16479e327dbc94c
tingru0515 <tingru0515@gmail.com>
2024-02-27 22:12:38 +1100

    error attribute

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,28 +12,34 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary __objects."""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)."""
         serialized = {}
-        for key, obj in self.__objects.items():
+        for key, obj in FileStorage.__objects.items():
             serialized[key] = obj.to_dict()
-        with open(self.__file_path, "w") as file:
+        with open(FileStorage.__file_path, "w") as file:
             json.dump(serialized, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects."""
         try:
-            with open(self.__file_path, "r") as file:
+            """with open(FileStorage.__file_path, "r") as file:
                 serialized = json.load(file)
                 for key, value in serialized.items():
                     class_name, obj_id = key.split('.')
-                    self.__objects[key] = eval(class_name)(**value)
+                    self.__objects[key] = eval(class_name)(**value)"""
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             pass
