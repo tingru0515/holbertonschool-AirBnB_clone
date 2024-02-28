41f2f82d11ea204081bf8a518a2451b890b05794
tingru0515 <tingru0515@gmail.com>
2024-02-28 12:09:17 +1100

    set property to be accessible

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,8 +7,24 @@ from models.base_model import BaseModel
 class FileStorage:
     """Serializes instances to JSON file and deserializes JSON file to instances."""
 
-    __file_path = "file.json"
+    __filepath = "file.json"
     __objects = {}
+
+    @property
+    def objects(self):
+        return FileStorage.__objects
+    
+    @objects.setter
+    def objects(self, value):
+        FileStorage.__objects = value
+
+    @property
+    def filepath(self):
+        return FileStorage.__filepath
+    
+    @filepath.setter
+    def filepath(self, value):
+        FileStorage.__filepath = value
 
     def all(self):
         """Returns the dictionary __objects."""
@@ -24,7 +40,7 @@ class FileStorage:
         serialized = {}
         for key, obj in FileStorage.__objects.items():
             serialized[key] = obj.to_dict()
-        with open(FileStorage.__file_path, "w") as file:
+        with open(FileStorage.__filepath, "w") as file:
             json.dump(serialized, file)
 
     def reload(self):
@@ -35,7 +51,7 @@ class FileStorage:
                 for key, value in serialized.items():
                     class_name, obj_id = key.split('.')
                     self.__objects[key] = eval(class_name)(**value)"""
-            with open(FileStorage.__file_path) as f:
+            with open(FileStorage.__filepath) as f:
                 objdict = json.load(f)
                 for o in objdict.values():
                     cls_name = o["__class__"]
