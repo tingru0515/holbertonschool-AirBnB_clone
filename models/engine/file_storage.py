987d9009388e450b6310d0562900b5316d11a30c
tingru0515 <tingru0515@gmail.com>
2024-02-28 12:16:29 +1100

    filepath

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,7 +7,7 @@ from models.base_model import BaseModel
 class FileStorage:
     """Serializes instances to JSON file and deserializes JSON file to instances."""
 
-    __filepath = "file.json"
+    __file_path = "file.json"
     __objects = {}
 
     @property
@@ -20,11 +20,11 @@ class FileStorage:
 
     @property
     def filepath(self):
-        return FileStorage.__filepath
+        return FileStorage.__file_path
     
     @filepath.setter
     def filepath(self, value):
-        FileStorage.__filepath = value
+        FileStorage.__file_path = value
 
     def all(self):
         """Returns the dictionary __objects."""
@@ -40,7 +40,7 @@ class FileStorage:
         serialized = {}
         for key, obj in FileStorage.__objects.items():
             serialized[key] = obj.to_dict()
-        with open(FileStorage.__filepath, "w") as file:
+        with open(FileStorage.__file_path, "w") as file:
             json.dump(serialized, file)
 
     def reload(self):
@@ -51,7 +51,7 @@ class FileStorage:
                 for key, value in serialized.items():
                     class_name, obj_id = key.split('.')
                     self.__objects[key] = eval(class_name)(**value)"""
-            with open(FileStorage.__filepath) as f:
+            with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
                 for o in objdict.values():
                     cls_name = o["__class__"]
