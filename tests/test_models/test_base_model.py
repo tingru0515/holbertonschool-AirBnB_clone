579038d05d4ea326659c4c4ea369f1813ea828e1
tingru0515 <tingru0515@gmail.com>
2024-02-28 21:05:11 +1100

    test basemodel save(self)

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@ import unittest
 import os
 from models.base_model import BaseModel
 from datetime import datetime
+from time import sleep
 
 class TestBaseModel(unittest.TestCase):
     def test_id_generation(self):
@@ -54,12 +55,58 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(model_dict['__class__'], 'BaseModel')
         print("to_dict contains all attributes test passed.")
 
-    def test_save_method(self):
-        # Test if save method creates a file
-        model = BaseModel()
-        model.save()
-        self.assertTrue(os.path.exists("file.json"))
-        print("Save method creates a file test passed.")
+class TestBaseModel_save(unittest.TestCase):
+    """Unittests for testing save method of the BaseModel class."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up test environment."""
+        try:
+            os.rename("file.json", "tmp")
+        except FileNotFoundError:
+            pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down test environment."""
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_one_save(self):
+        bm = BaseModel()
+        sleep(0.05)
+        first_updated_at = bm.updated_at
+        bm.save()
+        self.assertLess(first_updated_at, bm.updated_at)
+
+    def test_two_saves(self):
+        bm = BaseModel()
+        sleep(0.05)
+        first_updated_at = bm.updated_at
+        bm.save()
+        second_updated_at = bm.updated_at
+        self.assertLess(first_updated_at, second_updated_at)
+        sleep(0.05)
+        bm.save()
+        self.assertLess(second_updated_at, bm.updated_at)
+
+    def test_save_with_arg(self):
+        bm = BaseModel()
+        with self.assertRaises(TypeError):
+            bm.save()
+
+    def test_save_updates_file(self):
+        bm = BaseModel()
+        bm.save()
+        bmid = "BaseModel." + bm.id
+        with open("file.json", "r") as f:
+            self.assertIn(bmid, f.read())
 
 if __name__ == '__main__':
     unittest.main()
