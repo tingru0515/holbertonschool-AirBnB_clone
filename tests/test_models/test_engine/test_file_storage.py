1adcd44f052ca72d629f6b318d8f22b47b9c9c85
tingru0515 <tingru0515@gmail.com>
2024-02-28 19:59:39 +1100

    file storage test nested deeper

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Defines unittests for models/engine/file_storage.py.
+
+Unittest classes:
+    TestFileStorage
+"""
+
+
+import unittest
+import os
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+class TestFileStorage(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Set up test environment."""
+        cls.storage = FileStorage()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down test environment."""
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_new(self):
+        """Test adding a new object to storage."""
+        obj = BaseModel()
+        self.storage.new(obj)
+        self.assertIn("BaseModel.{}".format(obj.id), self.storage.all())
+
+    def test_save(self):
+        """Test saving objects to file."""
+        obj = BaseModel()
+        obj.name = "Test"
+        self.storage.new(obj)
+        self.storage.save()
+        with open("file.json", "r") as f:
+            data = f.read()
+            self.assertIn("BaseModel.{}".format(obj.id), data)
+
+    def test_reload(self):
+        """Test reloading objects from file."""
+        obj = BaseModel()
+        obj.name = "Test"
+        self.storage.new(obj)
+        self.storage.save()
+        self.storage.reload()
+        all_objs = self.storage.all()
+        self.assertIn("BaseModel.{}".format(obj.id), all_objs)
+
+if __name__ == '__main__':
+    unittest.main()
